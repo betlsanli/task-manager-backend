56d9b74a7c7bcbcf9828095ef95d0617ce799187
Betül Şanlı <betlsanli@gmail.com>
2024-12-07 16:20:33 +0300

    project controller mapping fix

--- src/main/java/com/example/tm/controllers/ProjectController.java
+++ src/main/java/com/example/tm/controllers/ProjectController.java
@@ -21,7 +21,7 @@ import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/tasklist")
+@RequestMapping("/project")
 @Validated
 public class ProjectController {
 
