2031b65aafdcd53f7afe779dab77e744c450fde1
Betül Şanlı <betlsanli@gmail.com>
2024-12-23 20:52:51 +0300

    tekrar özğr dilerim

--- src/main/java/com/example/tm/controllers/TaskController.java
+++ src/main/java/com/example/tm/controllers/TaskController.java
@@ -69,7 +69,7 @@ public class TaskController {
             if (userDetails.hasAuthority("ROLE_ADMIN")) {
                 return ResponseEntity.status(HttpStatus.OK).body(taskService.getTotalCount());
             }
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            return ResponseEntity.status(HttpStatus.OK).body(taskService.getTotalCountByUserId(userDetails.getUserId()));
         }
         catch (Exception e) {
             log.error(e.getMessage());
@@ -87,7 +87,7 @@ public class TaskController {
             if (userDetails.hasAuthority("ROLE_ADMIN") || projectAssignmentService.isProjectAssignedToUser(projectId, userDetails.getUserId())) {
                 return ResponseEntity.status(HttpStatus.OK).body(taskService.getTotalCountByProjectId(projectId));
             }
-            return ResponseEntity.status(HttpStatus.OK).body(taskService.getTotalCountByUserId(userDetails.getUserId()));
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
         catch (Exception e) {
             log.error(e.getMessage());
